b397ff373d888c84be4e4d5608f49d7efc0d31b1
Dante <dcn5783@g.rit.edu>
2016-07-30 16:10:06 -0700

    Rudimentary Monster AI created.

--- QuickBit Dungeon/QuickBit Dungeon/CORE/Monster.cs
+++ QuickBit Dungeon/QuickBit Dungeon/CORE/Monster.cs
@@ -11,7 +11,6 @@ namespace QuickBit_Dungeon.CORE
 		// ============== Members ===============
 		// ======================================
 
-		// Timing variables
 		public Timer AttackTimer { get; set; }
 		public Timer MoveTimer { get; set; }
 
@@ -22,9 +21,10 @@ namespace QuickBit_Dungeon.CORE
 			Hunt,
 			Wander
 		}
-
-		private int Sight { get; set; } = 5;
-		private int Range { get; set; } = 15;
+		
+		private Tuple<int, int> LastSeenPlayer { get; set; }
+		private int Sight { get; set; } = 3;
+		private int Range { get; set; } = 10;
 
 		// ======================================
 		// ============== Methods ===============
@@ -36,7 +36,8 @@ namespace QuickBit_Dungeon.CORE
 		public Monster() : base()
 		{
 			AttackTimer = new Timer(60);
-			MoveTimer = new Timer(80);
+			MoveTimer = new Timer(90);
+			LastSeenPlayer = new Tuple<int, int>(0, 0);
 		}
 
 		/// <summary>
@@ -48,6 +49,8 @@ namespace QuickBit_Dungeon.CORE
 			AttackTimer.Update();
 			MoveTimer.Update();
 
+			LookForPlayer();
+			CheckState();
 			switch (MonsterState)
 			{
 				case EMonsterState.Attack:
@@ -66,7 +69,7 @@ namespace QuickBit_Dungeon.CORE
 		/// Determines what state this monster
 		/// should currently be in.
 		/// </summary>
-		public void CheckState()
+		private void CheckState()
 		{
 			if (PlayerInSight())
 				MonsterState = EMonsterState.Attack;
@@ -76,14 +79,41 @@ namespace QuickBit_Dungeon.CORE
 				MonsterState = EMonsterState.Wander;
 		}
 
-		private bool PlayerInSight()
+		/// <summary>
+		/// If the player is in sight, the
+		/// monster remembers their position.
+		/// Used for movement.
+		/// </summary>
+		private void LookForPlayer()
 		{
-			return false;
+			if (PlayerInSight())
+				LastSeenPlayer = new Tuple<int, int>(Dungeon.MainPlayer.Y, Dungeon.MainPlayer.X);
 		}
 
+		/// <summary>
+		/// Returns whether or not the player
+		/// is in the monster's line of sight.
+		/// </summary>
+		/// <returns>Whether the player is in sight or not</returns>
+		private bool PlayerInSight()
+		{
+			return  Dungeon.MainPlayer.Y >= Y - Sight &&
+					Dungeon.MainPlayer.Y <= Y + Sight &&
+					Dungeon.MainPlayer.X >= X - Sight &&
+					Dungeon.MainPlayer.X <= X + Sight;
+		}
+
+		/// <summary>
+		/// Returns whether or not the player
+		/// is in the monster's range.
+		/// </summary>
+		/// <returns>Whether the player is in range or not</returns>
 		private bool PlayerInRange()
 		{
-			return false;
+			return  Dungeon.MainPlayer.Y >= Y - Range &&
+					Dungeon.MainPlayer.Y <= Y + Range &&
+					Dungeon.MainPlayer.X >= X - Range &&
+					Dungeon.MainPlayer.X <= X + Range;
 		}
 
 		/// <summary>
@@ -107,6 +137,8 @@ namespace QuickBit_Dungeon.CORE
 		public void HuntPlayer()
 		{
 			RegenHealth();
+			if (PlayerInRange())
+				MoveTowardPlayer();
 		}
 
 		/// <summary>
@@ -115,7 +147,8 @@ namespace QuickBit_Dungeon.CORE
 		/// </summary>
 		public void AttackPlayer()
 		{
-			
+			if (PlayerInSight())
+				MoveToPlayer();
 		}
 
 		/// <summary>
@@ -160,6 +193,87 @@ namespace QuickBit_Dungeon.CORE
 						Dungeon.MoveEntity(this, 0, -1);
 					MoveTimer.PerformAction();
 					break;
+			}
+		}
+
+		/// <summary>
+		/// Moves the monster in the last
+		/// direction that they saw the player
+		/// followed by random directions until
+		/// the player is no longer in range.
+		/// </summary>
+		private void MoveTowardPlayer()
+		{
+			// Find if there is a larger y or x difference
+			// between the monster's position and the player's.
+			// Whichever difference is larger is the the direction
+			// that the monster moves in.
+			var diffY = Math.Abs(LastSeenPlayer.Item1 - Y);
+			var diffX = Math.Abs(LastSeenPlayer.Item2 - X);
+
+			if (diffY > diffX && MoveTimer.ActionReady)
+				MoveInYDirection(LastSeenPlayer.Item1);
+			else if (diffY < diffX && MoveTimer.ActionReady)
+				MoveInXDirection(LastSeenPlayer.Item2);
+			else
+				MoveInYDirection(LastSeenPlayer.Item1);
+		}
+
+		/// <summary>
+		/// Moves the monster towards
+		/// the player's next location.
+		/// If they aren't there by the time
+		/// you have moved towards it,
+		/// move towards their exact location.
+		/// </summary>
+		private void MoveToPlayer()
+		{
+			// Find if there is a larger y or x difference
+			// between the monster's position and the player's.
+			// Whichever difference is larger is the the direction
+			// that the monster moves in.
+			var diffY = Math.Abs(Dungeon.MainPlayer.Y - Y);
+			var diffX = Math.Abs(Dungeon.MainPlayer.X - X);
+
+			if (diffY > diffX && MoveTimer.ActionReady)
+				MoveInYDirection(Dungeon.MainPlayer.Y);
+			else if (diffY < diffX && MoveTimer.ActionReady)
+				MoveInXDirection(Dungeon.MainPlayer.X);
+			else
+				MoveInYDirection(Dungeon.MainPlayer.Y);
+		}
+
+		/// <summary>
+		/// Moves the monster in the y direction.
+		/// </summary>
+		private void MoveInYDirection(int pointY)
+		{
+			if (pointY - Y > 0 && Dungeon.CanMove(this, 1, 0))
+			{
+				Dungeon.MoveEntity(this, 1, 0);
+				MoveTimer.PerformAction();
+			}
+			else if (pointY - Y < 0 && Dungeon.CanMove(this, -1, 0))
+			{
+				Dungeon.MoveEntity(this, -1, 0);
+				MoveTimer.PerformAction();
+			}
+		}
+
+		/// <summary>
+		/// Moves the monster in the x direction.
+		/// </summary>
+		private void MoveInXDirection(int pointX)
+		{
+			if (pointX - X > 0 && Dungeon.CanMove(this, 0, 1))
+			{
+				Dungeon.MoveEntity(this, 0, 1);
+				MoveTimer.PerformAction();
+			}
+			else if (pointX - X < 0 && Dungeon.CanMove(this, 0, -1))
+			{
+				Dungeon.MoveEntity(this, 0, -1);
+				MoveTimer.PerformAction();
 			}
 		}
 	}
